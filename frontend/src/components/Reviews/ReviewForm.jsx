b1f7f8494a7217b25b20f6b83789174b329d7ed0
Florencia <florenciamorelliIT@gmail.com>
2024-07-17 23:59:47 -0300

    reviews admin is fully working

--- frontend/src/components/Reviews/ReviewForm.jsx
+++ frontend/src/components/Reviews/ReviewForm.jsx
@@ -1,74 +1,118 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { addReview } from '../../redux/reducers/reviewsReducer';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useForm, Controller } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup';
+import { createReview, updateReview, readOneReview, readReviews } from '../../redux/reducers/reviewsReducer';
 
-const ReviewForm = () => {
+const reviewSchema = Yup.object().shape({
+    raceId: Yup.number().required('La carrera es requerida'),
+    userId: Yup.number().required('El usuario es requerido'),
+    rating: Yup.number().min(1).max(5).required('La puntuación es requerida'),
+    comment: Yup.string().required('El comentario es requerido'),
+});
+
+const ReviewForm = ({ id, handleCloseModal }) => {
     const dispatch = useDispatch();
-    const [race, setRace] = useState('');
-    const [user, setUser] = useState('');
-    const [rating, setRating] = useState('');
-    const [comment, setComment] = useState('');
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        dispatch(addReview({ race, user, rating, comment }));
-        // Clear form after submission
-        setUser('');
-        setRace('');
-        setRating('');
-        setComment('');
+    const races = useSelector((state) => state.races.data);
+    const users = useSelector((state) => state.users.data);
+
+    const { control, handleSubmit, setValue, reset, formState: { errors } } = useForm({
+        resolver: yupResolver(reviewSchema)
+    });
+
+    const editingObj = useSelector((state) => state.reviews.editingObj);
+
+    useEffect(() => {
+        if (id) {
+            dispatch(readOneReview(id));
+        } else {
+            reset(); // Restablecer el formulario al crear una nueva carrera
+        }
+    }, [dispatch, id, reset]);
+
+    useEffect(() => {
+        if (editingObj) {
+            setValue('raceId', editingObj.raceId);
+            setValue('userId', editingObj.userId);
+            setValue('rating', editingObj.rating);
+            setValue('comment', editingObj.comment);
+        }
+    }, [editingObj, setValue]);
+
+    const onSubmit = (data) => {
+        if (id) {
+            dispatch(updateReview({ ...data, id })).then(() => {
+                handleCloseModal();
+                dispatch(readReviews());
+            });
+        } else {
+            dispatch(createReview(data)).then(() => {
+                handleCloseModal();
+                dispatch(readReviews());
+            });
+        }
     };
 
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <div className="mb-3">
-                    <label htmlFor="race" className="form-label">Carrera</label>
-                    <input
-                        type="text"
-                        className="form-control"
-                        id="race"
-                        value={race}
-                        onChange={(e) => setRace(e.target.value)}
-                        required
-                    />
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="user" className="form-label">Usuario</label>
-                    <input
-                        type="text"
-                        className="form-control"
-                        id="user"
-                        value={user}
-                        onChange={(e) => setUser(e.target.value)}
-                        required
-                    />
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="rating" className="form-label">Puntuación</label>
-                    <input
-                        type="text"
-                        className="form-control"
-                        id="rating"
-                        value={rating}
-                        onChange={(e) => setRating(e.target.value)}
-                        required
-                    />
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="comment" className="form-label">Comentario</label>
-                    <input
-                        type="text"
-                        className="form-control"
-                        id="comment"
-                        value={comment}
-                        onChange={(e) => setComment(e.target.value)}
-                        required
-                    />
-                </div>
-                <button type="submit" className="btn btn-primary">Add Review</button>
-            </form>
-        </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="form-group">
+                <label>Carrera</label>
+                <Controller
+                    control={control}
+                    name="raceId"
+                    render={({ field }) => <select className={`form-select form-control ${errors.raceId ? 'is-invalid' : ''}`} {...field}>
+                        <option value="">Seleccione una carrera</option>
+                        {
+                            races && races.map((race) => {
+                                return(
+                                    <option value={ race.raceId }>{ race.raceName }</option>
+                                )
+                            })
+                        }
+                    </select>}
+                />
+                <div className="invalid-feedback">{errors.raceId?.message}</div>
+            </div>
+            <div className="form-group">
+                <label>Usuario</label>
+                <Controller
+                    control={control}
+                    name="userId"
+                    render={({ field }) => <select className={`form-select form-control ${errors.userId ? 'is-invalid' : ''}`} defaultValue={''} {...field}>
+                        <option value="">Seleccione un usuario</option>
+                    {
+                        users && users.map((user) => {
+                            return(
+                                <option value={ user.id }>{ user.username }</option>
+                            )
+                        })
+                    }
+                </select>}
+                />
+                <div className="invalid-feedback">{errors.userId?.message}</div>
+            </div>
+            <div className="form-group">
+                <label>Puntuación</label>
+                <Controller
+                    control={control}
+                    name="rating"
+                    render={({ field }) => <input type="number" min={1} max={5} className={`form-control ${errors.rating ? 'is-invalid' : ''}`} {...field} />}
+                />
+                <div className="invalid-feedback">{errors.rating?.message}</div>
+            </div>
+            <div className="form-group">
+                <label>Comentario</label>
+                <Controller
+                    control={control}
+                    name="comment"
+                    render={({ field }) => <input type="text" className={`form-control ${errors.comment ? 'is-invalid' : ''}`} {...field} />}
+                />
+                <div className="invalid-feedback">{errors.comment?.message}</div>
+            </div>
+            <button type="submit" className="btn btn-primary">Guardar</button>
+        </form>
     );
 };
 
