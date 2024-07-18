3daa555fd5c325a8ec938956cfd0e10235363d57
Florencia <florenciamorelliIT@gmail.com>
2024-07-18 13:39:28 -0300

    both login options are ready to make them function

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,25 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SignIn from '../components/SignIn/SignIn'
+import SignUp from '../components/SignUp/SignUp';
 import Button from 'react-bootstrap/esm/Button'
 
 function Login() {
+  const [activeLog, setActiveLog] = useState('signin');
+
+  const handleLogChange = (log) => {
+    setActiveLog(log);
+  }
+
+
   return (
     <div className='login'>
       <div className='titleDiv'>
-        <h1>Iniciar sesión</h1>
+        <h1>{activeLog === 'signin' ? 'Iniciar sesión' : 'Registrarse'}</h1>
       </div>
       <div className='textDiv'>
-        <p>
-          ¡Hola otra vez!
-          <br />
-          Ingresá a tu cuenta para acceder a las últimas reseñas de F1
-        </p>
-        <SignIn />
+        {
+          activeLog === 'signin'
+            ?
+            <p>
+              ¡Hola otra vez!
+              <br />
+              Ingresá a tu cuenta para acceder a las últimas reseñas de F1
+            </p>
+            :
+            <p>
+              ¡Bienvenido!
+              <br />
+              Creá tu cuenta para acceder a las últimas reseñas de F1
+            </p>
+        }
+        {activeLog === 'signin' ? <SignIn /> : <SignUp />}
       </div>
 
       <div className='footerDiv'>
-        <span className='text'>¿Aún no estás registrado?</span>
-        <Button variant='dark' size='sm'>Registrarme</Button>
+        {
+          activeLog === 'signin'
+            ?
+            <>
+              <p>¿No tenés una cuenta?</p>
+              <span onClick={() => handleLogChange('signup')}>Registrate</span>
+            </> 
+            : 
+            <>
+              <p>¿Ya tenés una cuenta?</p>
+              <span onClick={() => handleLogChange('signin')}>Iniciá sesión</span>
+            </>
+            }
       </div>
     </div>
   )
